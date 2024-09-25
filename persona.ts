22279ddbc444646f5bbd47e9b3ef4886f6750022
SebasMejia1 <sebastianmejiasanchez1030@gmail.com>
2024-09-25 10:48:40 -0500

    Ejercicio nro 6, se aplica el metodo saludar a la clase empleado y se añade el salario a el metodo

--- persona.ts
+++ persona.ts
@@ -7,14 +7,14 @@ export class Persona {
         this.edad = edad;
     }
 
-    saludar(){
+    saludar(): void{
         console.log('Tu nombre es : ', this.nombre, "Tu edad es : ", this.edad);
     }
 
-    getEdad(){
+    getEdad(): number {
         return this.edad;
     }
-
+ 
 }
 
 
@@ -24,8 +24,18 @@ class Empleado extends Persona {
         super(nombre, edad);
         this.salario = salario;
     }
-    trabajar(){
+    trabajar(): void{
         console.log(this.nombre, "Las horas que se trabajaran en el dia serán 8 horas");
     }
 
+    saludar(){
+        super.saludar();
+        console.log('El salario es : ', this.salario);
+    }
+    
+
 }
+let persona1 = new Empleado("Sebas", 22, 1200000)
+
+persona1.saludar();
+
